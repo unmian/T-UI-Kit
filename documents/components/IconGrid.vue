73fffbfb5c6e34f0d3cee1b517e82cec346c70b3
Quarter <master@shiruihua.cn>
2022-02-25 09:01:41 +0800

    更新图标库版本

--- documents/components/IconGrid.vue
+++ documents/components/IconGrid.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Quarter
  * @Date: 2021-12-31 02:17:49
- * @LastEditTime: 2022-02-21 08:45:40
+ * @LastEditTime: 2022-02-25 00:56:16
  * @LastEditors: Quarter
  * @Description: 图标网格
  * @FilePath: /t-ui-kit/documents/components/IconGrid.vue
@@ -36,16 +36,33 @@ const icons = computed(() => {
 });
 
 /**
- * @description: 处理点击事件
+ * @description: 处理辅助名称
  * @author: Quarter
  * @param {string} iconName 图标名称
  * @return
  */
-const handleClick = (iconName: string) => {
+const handleCopyName = (iconName: string) => {
   $clipboard
     ?.copy(iconName)
     .then(() => {
       $message?.success(`图标名称复制成功 ${iconName}`);
+    })
+    .catch((e) => {
+      $message?.success("复制失败，请再试一次或反馈工程师");
+    });
+};
+
+/**
+ * @description: 处理复制转义代码
+ * @author: Quarter
+ * @param {string} code 转义代码
+ * @return
+ */
+const handleCopyEntity = (code: string) => {
+  $clipboard
+    ?.copy(code)
+    .then(() => {
+      $message?.success(`图标转义代码复制成功 ${code}`);
     })
     .catch((e) => {
       $message?.success("复制失败，请再试一次或反馈工程师");
@@ -56,15 +73,16 @@ const handleClick = (iconName: string) => {
 <template>
   <div class="icon-grid">
     <ul>
-      <li
-        v-for="(icon, iconIndex) of icons"
-        :key="`icon-item-${iconIndex}`"
-        @click="handleClick(icon.name)"
-      >
+      <li v-for="(icon, iconIndex) of icons" :key="`icon-item-${iconIndex}`">
         <div class="icon-grid__icon">
           <t-icon :name="icon.name"></t-icon>
         </div>
-        <div class="icon_grid__name">{{ icon.name }}</div>
+        <div class="icon-grid__name">{{ icon.name }}</div>
+        <div class="icon-grid__operation">
+          <span @click="handleCopyName(icon.name)">名称</span>
+          <t-divider layout="vertical"></t-divider>
+          <span @click="handleCopyEntity(icon.entity)">代码</span>
+        </div>
       </li>
     </ul>
   </div>
@@ -85,12 +103,11 @@ const handleClick = (iconName: string) => {
     margin: 0;
 
     li {
-      width: 110px;
-      height: 110px;
+      width: 150px;
+      height: 120px;
       padding: var(--t-space-8);
       color: var(--t-text-gray-1);
       text-align: center;
-      cursor: pointer;
       box-sizing: border-box;
       border-radius: 6px;
       display: flex;
@@ -99,12 +116,9 @@ const handleClick = (iconName: string) => {
 
       .icon-grid__icon {
         @include t-text-1;
-
-        transform: scale(0.8) translateY(0);
-        transition: all 0.3s linear;
       }
 
-      .icon_grid__name {
+      .icon-grid__name {
         @include t-text-4;
 
         white-space: nowrap;
@@ -115,16 +129,31 @@ const handleClick = (iconName: string) => {
         margin-top: var(--t-space-4);
       }
 
-      &:hover {
-        background-color: var(--t-neutral-1-gray-2);
+      .icon-grid__operation {
+        @include t-text-4;
 
-        .icon-grid__icon {
-          transform: scale(1.2) translateY(13px);
+        color: var(--t-text-gray-1);
+        display: none;
+        margin-top: var(--t-space-4);
+
+        span {
+          cursor: pointer;
+
+          &:hover {
+            color: var(--t-brand-hover);
+          }
+        }
+      }
+
+      &:hover {
+        background-color: var(--t-neutral-1-gray-1);
+
+        .icon-grid__name {
+          display: none;
         }
 
-        .icon_grid__name {
-          opacity: 0;
-          transform: scale(0);
+        .icon-grid__operation {
+          display: block;
         }
       }
     }
